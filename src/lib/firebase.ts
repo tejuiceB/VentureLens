68d32f4ab8fc414bc0abd9266e0d8ce894f2d48a
manojvelamala <velamalamk@gmail.com>
2025-09-22 01:30:58 +0530

    Update firebase.ts
    Updated apiKey

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   "projectId": "studio-7464297256-ea848",
   "appId": "1:500487980857:web:5f56bd737633bac75bcae8",
-  "apiKey": "AIzaSyCwq7sAuDcKpbdI8m4FWm-grGRjgz8fssQ",
+  "apiKey": "XXXXXXXX",
   "authDomain": "studio-7464297256-ea848.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "500487980857"
